66cd849ed2483afba7b08caf604d99565c452ab0
Eelai Scheubel <189846219+Eelai-Scheubel@users.noreply.github.com>
2025-05-02 13:55:28 +0200

    Update code_g3.py

--- code_g3.py
+++ code_g3.py
@@ -3,10 +3,9 @@ from jobspy import scrape_jobs
 
 jobs = scrape_jobs(
     site_name=["indeed"],				#renseigner ici la palteforme souhaitée (indeed / linkedin / googlejobs)
-    search_term="data analyst",			#intitulé du poste recherché 
-    google_search_term="data analyst",	#intitulé du poste pour googlejobs
-    location="Paris",					#ville/ région 
-    results_wanted=40,					#nombre de résultats voulus
+    search_term='"alternance" AND ("data scientist" OR "data science" OR "data analyst" OR "data analyse" OR "quantitative" OR "statisticien")',			#intitulé du poste recherché
+    location="France",					#ville/ région 
+    results_wanted=200,					#nombre de résultats voulus
     hours_old=72,						#temps maximum depuis la publication (en heures)
     country_indeed='France',			#pays pour indeed 
    
